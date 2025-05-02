6fae13ec65c7f77ba87b7386150f9108d031254d
Toxa-K <originkewai666.com@mail.ru>
2025-05-02 11:11:59 +0300

    Add routing

--- index.js
+++ index.js
@@ -1,62 +1,32 @@
 const http = require('http');
 const dotenv = require('dotenv');
 const EventEmitter = require('events');
+const Router = require('./framework/router');
+
 
 dotenv.config();
 
 const event = new EventEmitter();
-
-// const server = http.createServer((req,res)=> {
-//     res.writeHead(200, {
-//         'content-type': 'text/html charset=utf-8'
-//     })
-//     if(req.url === "/user") {
-//         return res.end(JSON.stringify([{id:20, name: 'Pavel'}]))
-//     }
-// });
-
-// const PORT = process.env.PORT || 5000
-
-// server.listen(PORT, ()=> {
-//     console.log(`Server is starting on port: ${PORT}`);
-// })
-
-class Router {
-    constructor() {
-        this.endpoints = {
-
-        }
-    }
-
-    request(method = "GET", path, handler) {
-        if(!this.endpoints[path]) {
-            this.endpoints[path] = {};
-        }
-        const endpoint = this.endpoints[path];
-
-        if(endpoint[method]) {
-            throw new Error(`${method} по адресу ${path} уже существует`)
-        }
-
-        endpoint[method] = handler;
-
-        event.on(`[${path}]:[${method}]`, (req,res) => {
-            handler(req,res)
-        })
-    }
-
-    get(path, handler) {
-        this.request("GET", path, handler)
-    }
-    post(path, handler) {
-        this.request("POST", path, handler)
-    }
-    put(path, handler) {
-        this.request("PUT", path, handler)
-    }
-    delete(path, handler) {
-        this.request("DELETE", path, handler)
-    }
-}
+const PORT = process.env.PORT || 5000;
 
 const router = new Router();
+
+router.get('/user', (req,res)=> {
+    res.end('YOU SEND REQUEST TO /USER')
+})
+
+router.get('/posts', (req,res)=> {
+    res.end('YOU SEND REQUEST TO /POSTS')
+})
+
+const server = http.createServer((req,res) => {
+    const isEventReal = event.emit(`[${req.url}]:[${req.method}]`,req,res)
+    if(!isEventReal) {
+        res.end();
+    }
+    // res.end(req.url);
+})
+
+server.listen(PORT, ()=> {
+    console.log(`Server was start with port: ${PORT}`);
+})
